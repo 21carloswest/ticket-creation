6ebc3811cc733317a8d74791f4cb30abb6ca7866
21carloswest <143567983+21carloswest@users.noreply.github.com>
2023-12-30 18:48:22 -0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Ticket</title>
+  </head>
+  <body class="bg-dark text-light">
+
+    <div class="container">
+        <div class="jumbotron bg-danger">
+           <h1>AAAAAAAAAAAAAAAAA</h1> 
+        </div>
+
+    
