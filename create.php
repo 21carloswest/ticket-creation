5ee7fb52ea3974ddfb4ea7cd58c7ee5f1d5c4e2d
21carloswest <143567983+21carloswest@users.noreply.github.com>
2024-01-01 21:28:13 -0300

    Update create.php
    alterações feitas no dia 27/12

--- create.php
+++ create.php
@@ -1,4 +1,25 @@
 <?php
+
+require __DIR__."/vendor/autoload.php";
+
 include __DIR__."/includes/header.php";
 include __DIR__."/includes/form.php";
 include __DIR__."/includes/footer.php";
+
+use \App\Entity\Ticket;
+use \App\Db\Database;
+use App\Entity\Description;
+
+if(isset($_POST["title"], /*$_POST['description'], $_POST['status'], $_POST['sys'], $_POST['SLA'], $_POST["author"], $_POST["costumer"], $_POST["tag"]*/)) {
+    $ticket = new Ticket($_POST["title"], $_POST['description'], $_POST['status'], $_POST['sys'], $_POST['SLA'], $_POST["GCM"], $_POST["author"], $_POST["costumer"], $_POST["tag"]);
+    $ticket->createTicket();
+
+    $desc = new Description($_POST['description']);
+    $desc->setIdTicket($ticket->getID());
+    $desc->createDescription();
+
+
+    
+    
+
+}
