70c00e91c75cbf45f5f0448d1fbb63fdfc2da09d
21carloswest <143567983+21carloswest@users.noreply.github.com>
2024-01-01 21:45:50 -0300

    Update list.php
    feito dia 28-12

--- includes/list.php
+++ includes/list.php
@@ -1,1 +1,44 @@
-        <a href = "create.php"><button class="btn btn-success">Novo</button></a>
+<?php
+use \App\Entity\Ticket;
+$consTicket = Ticket::getTickets();
+$resultado = '';
+
+foreach($consTicket as $ticket){
+  $resultado .= "<tr onclick="."window.location='viewTicket.php?id="."$ticket->id'".">
+                    <td>$ticket->id</td>
+                    <td>$ticket->titulo</td>
+                    <td>$ticket->idStatus</td>
+                    <td>$ticket->idResponsavel</td>
+                  </tr>";
+}
+
+?>
+
+<a href = "create.php">
+    <button class="btn btn-success">Novo</button>
+</a>
+
+<a>
+    <button class="btn btn-light">Buscar ticket</button>
+</a>
+
+<section>
+
+    <table class="table bg-light mt-3">
+
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Status</th>
+                <th>Responsável</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?= $resultado;?>
+
+        </tbody>
+    </table>
+
+</section>
