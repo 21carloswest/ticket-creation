5c433a9be4c697132658cf313388f77a86da1a8b
Matheus Eterno <matheus.eterno@outlook.com.br>
2024-01-08 17:57:12 -0300

    Criação das telas modal para substituir as páginas de cadastro

--- category/viewCategory.php
+++ category/viewCategory.php
@@ -3,6 +3,7 @@
 require __DIR__."/../vendor/autoload.php";
 
 use \App\Db\Select;
+use \App\Entity\Category;
 
 $aftermath = "";
 
@@ -15,6 +16,15 @@ foreach($consultaCategory as $category){
                     <td>".($category->ATIVO_CATEGORIA == 1 ? 'Ativo' : 'Inativo')."</td>
                     <td><i class='bi bi-pencil'  style='cursor: pointer;' onclick="."window.location='editCategory.php?id=$category->ID_CATEGORIA'"."></i></td>
                   </tr>";
+}
+
+if(isset($_POST["DESCRICAO_CATEGORIA"])){
+  $category = new Category ($_POST["DESCRICAO_CATEGORIA"]);
+  $category->createCategory();
+
+  header("location: /ticket-creation/index.php?status=success");
+
+  exit;
 }
 
 include __DIR__."/../includes/header.php";
--- includes/macro/macroList.php
+++ includes/macro/macroList.php
@@ -1,7 +1,33 @@
 <section>
 
-    <button class="btn btn-success" onclick="window.location='newMacro.php'">Nova</button>
+    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Nova</button>
     <button type="button" class="btn btn-primary me-2" onclick="window.location='/ticket-creation'">Voltar</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastro de Macro</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Titulo:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Mensagem da Macro:</label>
+                            <textarea class="form-control" id="message-text" rows=12></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Salvar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <table class="table bg-light mt-3">
 
