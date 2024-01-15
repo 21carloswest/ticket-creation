9a9fd59aacda818bf63c89bba07f46bb62221169
21carloswest <143567983+21carloswest@users.noreply.github.com>
2024-01-15 19:43:15 -0300

    Criação dos models e controllers

---
+++ database/migrations/2024_01_15_173506_create_tickets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Categoria;
+use App\Models\Cliente;
+use App\Models\Sistema;
+use App\Models\Sla;
+use App\Models\Status;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Cliente::class);
+            $table->foreignIdFor(Categoria::class);
+            $table->foreignIdFor(Sla::class);
+            $table->foreignIdFor(Sistema::class);
+            $table->foreignIdFor(Status::class);
+            $table->foreignIdFor(Tag::class);
+            $table->string('titulo');
+            $table->integer('gcm');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
