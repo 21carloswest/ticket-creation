0c00c44bdad103e103eafc146c32ed3a57ba7a2f
21carloswest <143567983+21carloswest@users.noreply.github.com>
2024-01-23 18:53:01 -0300

    Criação de resources

--- database/migrations/2024_01_15_173319_create_macros_table.php
+++ database/migrations/2024_01_15_173319_create_macros_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('titulo');
             $table->text('texto');
+            $table->boolean('ativo');
             $table->timestamps();
         });
     }
